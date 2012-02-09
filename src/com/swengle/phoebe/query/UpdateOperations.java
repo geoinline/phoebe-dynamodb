7540424656c2fe162f9e7775f31a690d2e521c8c
btoc007@gmail.com <btoc007@gmail.com@d1e74156-f561-450b-851c-e6714339b34d>
2012-02-09 18:14:10 +0000

    Removed public identifier, not required

--- src/com/swengle/phoebe/query/UpdateOperations.java
+++ src/com/swengle/phoebe/query/UpdateOperations.java
@@ -10,22 +10,22 @@ package com.swengle.phoebe.query;
  */
 public interface UpdateOperations<T> {
 	/** adds the val to a set field **/
-	public UpdateOperations<T> add(String fieldName, Object val);
+	UpdateOperations<T> add(String fieldName, Object val);
 	/** adds the vals to a set field **/
-	public UpdateOperations<T> addAll(String fieldName, Iterable<?> vals);
+	UpdateOperations<T> addAll(String fieldName, Iterable<?> vals);
 	/** decrements the number field by 1 **/
-	public UpdateOperations<T> dec(String fieldName);
+	UpdateOperations<T> dec(String fieldName);
 	/** increments the number field by 1 **/
-	public UpdateOperations<T> inc(String fieldName);
+	UpdateOperations<T> inc(String fieldName);
 	/** increments the number field by value (negative values allowed) **/
-	public UpdateOperations<T> inc(String fieldName, Number value);
+	UpdateOperations<T> inc(String fieldName, Number value);
 	/** removes val from the set field **/
-	public UpdateOperations<T> remove(String fieldName, Object val);
+	UpdateOperations<T> remove(String fieldName, Object val);
 	/** removes vals from the set field **/
-	public UpdateOperations<T> removeAll(String fieldName, Iterable<?> vals);
+	UpdateOperations<T> removeAll(String fieldName, Iterable<?> vals);
 	/** sets the field to val **/
-	public UpdateOperations<T> set(String fieldName, Object val);
+	UpdateOperations<T> set(String fieldName, Object val);
 	/** removes the field **/
-	public UpdateOperations<T> unset(String fieldName);
+	UpdateOperations<T> unset(String fieldName);
 
 }
