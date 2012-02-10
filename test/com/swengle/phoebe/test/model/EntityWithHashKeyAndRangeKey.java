592794314e41a102797a8f52d61ec50ee46b439a
btoc007@gmail.com <btoc007@gmail.com@d1e74156-f561-450b-851c-e6714339b34d>
2012-02-10 18:47:39 +0000

    Better naming of test entities

---
+++ test/com/swengle/phoebe/test/model/EntityWithHashKeyAndRangeKey.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package com.swengle.phoebe.test.model;
+
+import java.util.Set;
+
+import com.amazonaws.services.dynamodb.datamodeling.DynamoDBAttribute;
+
+/**
+ * @author Brian O'Connor <btoc008@gmail.com>
+ *
+ */
+public class EntityWithHashKeyAndRangeKey extends EntityWithJustHashKeyAndJustRangeKey {
+	private String string;
+	private int number;
+	private Set<String> set;
+	
+	/**
+	 * 
+	 */
+	public EntityWithHashKeyAndRangeKey() {
+		// nothing to do
+	}
+
+	/**
+	 * @return the string
+	 */
+	@DynamoDBAttribute(attributeName="0")
+	public String getString() {
+		return string;
+	}
+
+	/**
+	 * @param string the string to set
+	 */
+	public void setString(String string) {
+		this.string = string;
+	}
+
+	/**
+	 * @return the number
+	 */
+	@DynamoDBAttribute(attributeName="1")
+	public int getNumber() {
+		return number;
+	}
+
+	/**
+	 * @param number the number to set
+	 */
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	/**
+	 * @return the set
+	 */
+	@DynamoDBAttribute(attributeName="3")
+	public Set<String> getSet() {
+		return set;
+	}
+
+	/**
+	 * @param set the set to set
+	 */
+	public void setSet(Set<String> set) {
+		this.set = set;
+	}
+
+
+}
