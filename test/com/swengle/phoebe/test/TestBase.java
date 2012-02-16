1b6a767749c8780b647263b978d437e1d3953ab7
btoc007@gmail.com <btoc007@gmail.com@d1e74156-f561-450b-851c-e6714339b34d>
2012-02-16 17:55:40 +0000

    Changed to AWS 1.3.2

--- test/com/swengle/phoebe/test/TestBase.java
+++ test/com/swengle/phoebe/test/TestBase.java
@@ -4,29 +4,19 @@
 package com.swengle.phoebe.test;
 
 import java.io.IOException;
-import java.lang.reflect.Method;
-import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.junit.Before;
 import org.junit.BeforeClass;
 
-import com.amazonaws.AmazonServiceException;
 import com.amazonaws.ClientConfiguration;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.PropertiesCredentials;
-import com.amazonaws.services.dynamodb.model.CreateTableRequest;
-import com.amazonaws.services.dynamodb.model.DeleteTableRequest;
-import com.amazonaws.services.dynamodb.model.DescribeTableRequest;
-import com.amazonaws.services.dynamodb.model.KeySchema;
-import com.amazonaws.services.dynamodb.model.KeySchemaElement;
-import com.amazonaws.services.dynamodb.model.ProvisionedThroughput;
-import com.amazonaws.services.dynamodb.model.TableDescription;
-import com.amazonaws.services.dynamodb.model.TableStatus;
 import com.swengle.phoebe.Phoebe;
-import com.swengle.phoebe.key.EntityKey;
-import com.swengle.phoebe.reflect.DynamoDBReflector;
+import com.swengle.phoebe.concurrent.FutureResult;
+import com.swengle.phoebe.datastore.DuplicateTableException;
+import com.swengle.phoebe.query.Query;
 import com.swengle.phoebe.test.model.EntityWithJustHashKey;
 import com.swengle.phoebe.test.model.EntityWithJustHashKeyAndJustRangeKey;
 
@@ -41,8 +31,9 @@ public class TestBase {
 
 	
 	@BeforeClass
-	public static void setUpTables() throws IOException {
+	public static void globalSetup() throws IOException {
 		ClientConfiguration clientConfiguration = new ClientConfiguration();
+		
 		AWSCredentials credentials = new PropertiesCredentials(
 				TestBase.class.getClassLoader().getResourceAsStream("AwsCredentials.properties"));
 		PHOEBE = new Phoebe(credentials, clientConfiguration);
@@ -63,152 +54,32 @@ public class TestBase {
 
 	@SuppressWarnings("unused")
 	private void deleteTestTables() {
-		deleteTable(DynamoDBReflector.INSTANCE.getTable(EntityWithJustHashKey.class).tableName());
-		deleteTable(DynamoDBReflector.INSTANCE.getTable(EntityWithJustHashKeyAndJustRangeKey.class).tableName());
+		PHOEBE.getDatastore().dropTable(EntityWithJustHashKey.class);
+		PHOEBE.getDatastore().dropTable(EntityWithJustHashKeyAndJustRangeKey.class);
 	}
 
 
 	private static void createTestTables() {
-		Method getter = DynamoDBReflector.INSTANCE.getHashKeyGetter(EntityWithJustHashKey.class);
-		String entityWithHashKeyAttrName = DynamoDBReflector.INSTANCE.getAttributeName(getter);
-		getter = DynamoDBReflector.INSTANCE.getHashKeyGetter(EntityWithJustHashKeyAndJustRangeKey.class);
-		String entityWithHashKeyAndRangeKeyHashAttrName = DynamoDBReflector.INSTANCE.getAttributeName(getter);
-		getter = DynamoDBReflector.INSTANCE.getRangeKeyGetter(EntityWithJustHashKeyAndJustRangeKey.class);
-		String entityWithHashKeyAndRangeKeyRangeAttrName = DynamoDBReflector.INSTANCE.getAttributeName(getter);
-		
-		
-		createTable(EntityWithJustHashKey.class, 128L, 128L,
-				entityWithHashKeyAttrName, "S");
-		createTable(EntityWithJustHashKeyAndJustRangeKey.class, 128L, 128L,
-				entityWithHashKeyAndRangeKeyHashAttrName, "S",
-				entityWithHashKeyAndRangeKeyRangeAttrName, "S");
-	}
-	
-	private void deleteTable(String tableName) {
-		DeleteTableRequest deleteTableRequest = new DeleteTableRequest().withTableName(tableName);
+		FutureResult<Void> create1FutureResult = PHOEBE.getAsyncDatastore().createTable(EntityWithJustHashKey.class, 128L, 128L);
+		FutureResult<Void> create2FutureResult = PHOEBE.getAsyncDatastore().createTable(EntityWithJustHashKeyAndJustRangeKey.class, 128L, 128L);
 		try {
-			PHOEBE.getClient().deleteTable(deleteTableRequest);
-		} catch (AmazonServiceException ase) {
-			if (ase.getErrorCode().equalsIgnoreCase(
-					"ResourceNotFoundException") == false) {
-				throw ase;
-			} else {
-				return;
-			}
+			create1FutureResult.now();
+		} catch (DuplicateTableException e) {
+			LOG.warn(e);
 		}
-		waitForTableToBecomeDelete(tableName);
-	}
-
-	private static <T> void createTable(Class<T> kindClass, long readCapacityUnits,
-			long writeCapacityUnits, String hashKeyName, String hashKeyType) {
-
-		createTable(kindClass, readCapacityUnits, writeCapacityUnits,
-				hashKeyName, hashKeyType, null, null);
-	}
-
-	private static <T> void createTable(Class<T> kindClass, long readCapacityUnits,
-			long writeCapacityUnits, String hashKeyName, String hashKeyType,
-			String rangeKeyName, String rangeKeyType) {
-
-		String tableName = DynamoDBReflector.INSTANCE.getTable(kindClass).tableName();
 		try {
-			KeySchemaElement hashKey = new KeySchemaElement()
-					.withAttributeName(hashKeyName).withAttributeType(
-							hashKeyType);
-			KeySchema ks = new KeySchema().withHashKeyElement(hashKey);
-
-			if (rangeKeyName != null) {
-				KeySchemaElement rangeKey = new KeySchemaElement()
-						.withAttributeName(rangeKeyName).withAttributeType(
-								rangeKeyType);
-				ks.setRangeKeyElement(rangeKey);
-			}
-
-			// Provide initial provisioned throughput values as Java long data
-			// types
-			ProvisionedThroughput provisionedthroughput = new ProvisionedThroughput()
-					.withReadCapacityUnits(readCapacityUnits)
-					.withWriteCapacityUnits(writeCapacityUnits);
-
-			CreateTableRequest request = new CreateTableRequest()
-					.withTableName(tableName).withKeySchema(ks)
-					.withProvisionedThroughput(provisionedthroughput);
-
-			PHOEBE.getClient().createTable(request);
-			waitForTableToBecomeAvailable(tableName);
-
-		} catch (AmazonServiceException ase) {
-			if (ase.getMessage().indexOf("Duplicate table name") == -1) {
-				LOG.error("Failed to create table " + tableName, ase);
-				System.exit(1);
-			} else {
-				LOG.info(tableName + " already exists, no need to create");
-			}
+			create2FutureResult.now();
+		} catch (DuplicateTableException e) {
+			LOG.warn(e);
 		}
 	}
 	
 	private <T> void empty(Class<T> kindClass) {
 		LOG.info("Emptying the table for: " + kindClass);
-		List<EntityKey<T>> objectKeys = PHOEBE.createScanQuery(kindClass).asEntityKeyList();
-		LOG.info("Found " + objectKeys.size() + " entities to remove.");
-		PHOEBE.getAsyncDatastore().delete(objectKeys).now();
+		Query<T>  qry = PHOEBE.createScanQuery(kindClass);
+		LOG.info("Found " + qry.count() + " entities to remove.");
+		PHOEBE.getDatastore().delete(qry);
 		LOG.info("Done emptying the table for: " + kindClass);
-	}
-
-	private static void waitForTableToBecomeAvailable(String tableName) {
-		LOG.info("Waiting for " + tableName + " to become ACTIVE...");
-
-		long startTime = System.currentTimeMillis();
-		long endTime = startTime + (10 * 60 * 1000);
-		while (System.currentTimeMillis() < endTime) {
-			try {
-				Thread.sleep(1000 * 20);
-			} catch (Exception e) {
-			}
-			try {
-				DescribeTableRequest request = new DescribeTableRequest()
-						.withTableName(tableName);
-				TableDescription tableDescription = PHOEBE.getClient()
-						.describeTable(request).getTable();
-				String tableStatus = tableDescription.getTableStatus();
-				LOG.info("  - current state: " + tableStatus);
-				if (tableStatus.equals(TableStatus.ACTIVE.toString()))
-					return;
-			} catch (AmazonServiceException ase) {
-				if (ase.getErrorCode().equalsIgnoreCase(
-						"ResourceNotFoundException") == false)
-					throw ase;
-			}
-		}
-		throw new RuntimeException("Table " + tableName + " never went active");
-	}
-
-	
-	private void waitForTableToBecomeDelete(String tableName) {
-		LOG.info("Waiting for " + tableName + " to become DELETED...");
-
-		long startTime = System.currentTimeMillis();
-		long endTime = startTime + (10 * 60 * 1000);
-		while (System.currentTimeMillis() < endTime) {
-			try {
-				Thread.sleep(1000 * 20);
-			} catch (Exception e) {
-			}
-			try {
-				DescribeTableRequest request = new DescribeTableRequest()
-						.withTableName(tableName);
-				PHOEBE.getClient()
-						.describeTable(request).getTable();
-			} catch (AmazonServiceException ase) {
-				if (ase.getErrorCode().equalsIgnoreCase(
-						"ResourceNotFoundException") == false)
-					throw ase;
-				else {
-					return;
-				}
-			}
-		}
-		throw new RuntimeException("Table " + tableName + " never went deleted");
 	}
 	
 }
